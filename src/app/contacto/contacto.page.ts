d4a5ea1da5eb6a9ca2643203f4b9ee87b7c0437b
AlexisWPSAguirre <camiloaguirrebastidas@gmail.com>
2021-03-13 10:56:18 -0500

    Saludar y Aritmetica

--- src/app/contacto/contacto.page.ts
+++ src/app/contacto/contacto.page.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController, AlertController } from '@ionic/angular'
+
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.page.html',
   styleUrls: ['./contacto.page.scss'],
 })
-export class ContactoPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ContactoPage{
+    nombre = "";
+    constructor(public alertController: AlertController) {}
+    async alertaNombre() {
+      if(this.nombre == ""){
+        const alert = await this.alertController.create({
+          cssClass: 'my-custom-class',
+          header: 'Saludo',
+          message: 'Por favor ingrese un nombre para saludar ',
+          buttons: ['Ok']
+        });
+        
+        await alert.present();
+      }
+      else
+      {
+      const alert = await this.alertController.create({
+        cssClass: 'my-custom-class',
+        header: 'Saludo',
+        message: 'Bienvenido '+this.nombre,
+        buttons: ['Ok']
+      });
+      await alert.present();
+      this.nombre = "";
+    }
   }
 
 }
--- src/app/nosotros/nosotros.page.ts
+++ src/app/nosotros/nosotros.page.ts
@@ -6,7 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nosotros.page.scss'],
 })
 export class NosotrosPage implements OnInit {
+  num1 = null;
+  num2 = null;
+  operacion = null;
+  r = null;
+  strOperacion = null;
+  mensaje = null;
+  
+  
+  operar(){
+    let n1 = parseInt(this.num1)
+    let n2 = parseInt(this.num2)
+    let n3 = parseInt(this.r)
+    switch(this.operacion)
+    {
+      case '+':
+        this.strOperacion = 'suma'
+        this.r = n1 + n2
+      break;
 
+      case '-':
+        this.strOperacion = 'resta'
+        this.r = n1 - n2
+      break;
+
+      case '*':
+        this.strOperacion = 'multiplicación'
+        this.r = n1 * n2
+      break;
+
+      case '/':
+        this.strOperacion = 'división'
+        this.r = n1 / n2
+      break;
+    }
+    this.mensaje = `El resultado de la ${this.strOperacion} entre ${this.num1} y ${this.num2} es igual a ${this.r}` ;
+  } 
   constructor() { }
 
   ngOnInit() {
